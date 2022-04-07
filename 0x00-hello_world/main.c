17aec902456d0aeb494ba9f417bb1fa100093998
Kenny <bankejesukenny@gmail.com>
2022-04-07 04:44:41 -0700

    Added task 0-6

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,1 +1,11 @@
-#include <stdio.h>\n\n /**\n* main - Entry point\n*\n* Return: Always 0 (Success)\n*/\nint main(void)\n{\n    return (0);\n}
+#include <stdio.h>
+
+ /**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+    return (0);
+}
