37fb6e676fb6294074d722b412cbe9125eee1fcd
Khenzidon <77622059+Khenzidon@users.noreply.github.com>
2022-04-08 15:22:06 -0700

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+/**
+ * main - prints the alphabet in lowercase,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+  char ch = 'a'
+    while (ch <= 'z')
+    {
+      if (ch != 'e' && ch != 'q')
+        putchar(ch);
+    }
+  putchar(\n);
+  return (0);
+}
