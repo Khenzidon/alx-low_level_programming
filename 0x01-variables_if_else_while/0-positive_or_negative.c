dda1b44311e63c27c69fe31c3117ab9dc8785afd
Khenzidon <77622059+Khenzidon@users.noreply.github.com>
2022-04-08 09:08:13 -0700

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,27 +1,14 @@
 #include <stdlib.h>
-
 #include <time.h>
-
 /* more headers goes there */
 
-
-
 /* betty style doc for function main goes there */
-
 int main(void)
-
 {
+	int n;
 
-README.md int n;
-
-
-
-README.md srand(time(0));
-
-README.md n = rand() - RAND_MAX / 2;
-
-README.md /* your code goes there */
-
-README.md return (0);
-
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	return (0);
 }
