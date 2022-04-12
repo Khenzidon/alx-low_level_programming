27fb1e548e4058570486ad281b733d56ff82e366
Kenny <bankejesukenny@gmail.com>
2022-04-12 09:39:29 -0700

    commit

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>                                                                                                                                     
+
+                                                                                                                                                       
+
+/**                                                                                                                                                    
+
+ * main - prints out _putchar                                                                                                                          
+
+ * Return 0 if executed properly                                                                                                                       
+
+*/                                                                                                                                                     
+
+int main(void)                                                                                                                                         
+
+{                                                                                                                                                      
+
+        putchar(_putchar);                                                                                                                             
+
+        return 0;                                                                                                                                      
+
+}
