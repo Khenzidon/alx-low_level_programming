0cb5307f8b2bb3d4f5470a89ae79682bc8dc499d
Khenzidon <77622059+Khenzidon@users.noreply.github.com>
2022-04-13 08:28:30 -0700

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,1 +1,11 @@
-4-isalpha.c
+#include "holberton.h"
+
+/**
+ * _islower - checks for lowercase character
+ * @c: the character to check
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
