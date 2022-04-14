acbcbcd845f5ce511b09b1f38c13d266134039b1
Khenzidon <77622059+Khenzidon@users.noreply.github.com>
2022-04-14 11:04:01 -0700

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,26 +1,16 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
 #include "main.h"
-/* more headers goes there */
 
 /**
- * main - assigns a random number to int n everytime
- * it executes, and prints it
- * Return: Always 0 (Success)n
- */
-int main(void)
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
 void positive_or_negative(int i)
 {
-	int n;
+        int i;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n == 0)
-		printf("%d is zero\n", n);
-	else if (n < 0)
-		printf("%d is negative\n", n);
-	return (0);
+        i = 98;
+        positive_or_negative(i);
+
+        return (0);
 }
