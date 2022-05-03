8156102da5e17137641f7b5d7481a070ecfd6cdf
Khenzidon <77622059+Khenzidon@users.noreply.github.com>
2022-05-03 12:00:55 -0700

    Update 6-abs.c

--- 0x09-static_libraries/6-abs.c
+++ 0x09-static_libraries/6-abs.c
@@ -1,12 +1,18 @@
 #include "main.h"
 
 /**
- * _abs - return abs value
- * @n:number to check
- * Return:0 or 1
+ * _abs - check the code for alx School students.
+ *
+ * @n: is an integer argument
+ *
+ * Return: Always 0.
  */
 
 int _abs(int n)
 {
-	return (n >= 0 ? n : n * -1);
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
 }
