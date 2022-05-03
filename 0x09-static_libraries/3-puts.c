e1c029563a56a62ef5e7b2a022f899b663bb2230
Kenny <bankejesukenny@gmail.com>
2022-05-03 11:32:48 -0700

    commit

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,18 +1,17 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
- * _puts - prints a string, followed by a new line,
+ * _puts - prints a string to stdout
  * @str: pointer to the string to print
- * Return: void
-*/
-
-
+ */
 void _puts(char *str)
 {
-int i = 0;
-while (str[i])
-{
-	_putchar(str[i]);
-	i++;
-}
-_putchar('\n');
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		putchar(str[i]);
+	}
+	putchar('\n');
 }
